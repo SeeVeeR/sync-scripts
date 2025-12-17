22223b1241356f5f473c4e00f6a4c6530d0b2496
Sergey <you@example.com>
2025-12-17 23:04:40 +0300

    Extract rsync options into variable

--- sync.sh
+++ sync.sh
@@ -11,29 +11,32 @@ SRC_SERVER="/mnt/storage/network_storage"
 LINUX_BOOKS="программирование/Linux/книги/"
 LINUX_VIDEO="программирование/Linux/видео/"
 
+# Опции rsync (одно место для настройки)
+RSYNC_OPTS="-avz --delete"
+
 ###########################################################
 
 sync_folder() {
-  FOLDER="$1"
+  local FOLDER="$1"
 
   echo
   echo "Синхронизация папки $FOLDER"
   read -p "Если всё ок, нажми Enter для dry-run..."
 
-  rsync -avz --delete --dry-run -e ssh \
+  rsync $RSYNC_OPTS --dry-run -e ssh \
     "$SRC_MINT/$FOLDER" \
     "$USER@$HOST:$SRC_SERVER/$FOLDER"
 
   echo
   read -p "Если всё ок, нажми Enter для реальной синхронизации..."
 
-  rsync -avz --delete -e ssh \
+  rsync $RSYNC_OPTS -e ssh \
     "$SRC_MINT/$FOLDER" \
     "$USER@$HOST:$SRC_SERVER/$FOLDER"
 }
 
 ###########################################################
-# Вызовы функции
+# Вызовы
 
 sync_folder "$LINUX_BOOKS"
 sync_folder "$LINUX_VIDEO"
