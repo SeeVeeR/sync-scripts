887abfd3e07a436ed08c71dffd0e5bb72bf9d60d
Sergey <you@example.com>
2025-12-17 16:30:03 +0300

    Initial commit: rsync sync scripts

---
+++ sync.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+USER="svr"
+HOST="192.168.0.13"
+
+# Корневые папки
+SRC_MINT="/data/network_storage"
+SRC_SERVER="/mnt/storage/network_storage"
+
+# Папки
+LINUX_BOOKS="программирование/Linux/книги/"
+LINUX_VIDEO="программирование/Linux/видео/"
+
+###########################################################
+
+sync_folder() {
+  FOLDER="$1"
+
+  echo
+  echo "Синхронизация папки $FOLDER"
+  read -p "Если всё ок, нажми Enter для dry-run..."
+
+  rsync -avz --delete --dry-run -e ssh \
+    "$SRC_MINT/$FOLDER" \
+    "$USER@$HOST:$SRC_SERVER/$FOLDER"
+
+  echo
+  read -p "Если всё ок, нажми Enter для реальной синхронизации..."
+
+  rsync -avz --delete -e ssh \
+    "$SRC_MINT/$FOLDER" \
+    "$USER@$HOST:$SRC_SERVER/$FOLDER"
+}
+
+###########################################################
+# Вызовы функции
+
+sync_folder "$LINUX_BOOKS"
+sync_folder "$LINUX_VIDEO"
