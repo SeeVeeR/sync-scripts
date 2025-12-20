4257b405125c4e2a492a1bb693411d438a760e1f
Sergey <you@example.com>
2025-12-20 05:43:15 +0300

    Extract message prefixes into variables

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# TODO: Добавить папку "Документы" для синхронизации
 
 USER="svr"
 HOST="192.168.0.13"
@@ -10,9 +11,17 @@ SRC_SERVER="/mnt/storage/network_storage"
 # Папки
 LINUX_BOOKS="программирование/Linux/книги/"
 LINUX_VIDEO="программирование/Linux/видео/"
+OBSIDIAN="obsidian/"
 
 # Опции rsync (одно место для настройки)
 RSYNC_OPTS="-avz --delete"
+
+# Префиксы сообщений
+MSG_INFO="[INFO]"
+MSG_CHECK="[CHECK]"
+MSG_RUN="[RUN]"
+MSG_OK="[OK]"
+
 
 ###########################################################
 
@@ -20,9 +29,9 @@ sync_folder() {
   local FOLDER="$1"
 
   echo
-  echo "[INFO] Синхронизация папки: $FOLDER"
+  echo "$MSG_INFO Синхронизация папки: $FOLDER"
 
-  echo "[CHECK] Dry-run. Изменения НЕ применяются."
+  echo "$MSG_CHECK Dry-run. Изменения НЕ применяются."
   read -p "Нажми Enter для выполнения dry-run..."
 
   rsync $RSYNC_OPTS --dry-run -e ssh \
@@ -30,19 +39,21 @@ sync_folder() {
     "$USER@$HOST:$SRC_SERVER/$FOLDER"
 
   echo
-  echo "[RUN] Реальная синхронизация."
+  echo "$MSG_RUN Реальная синхронизация."
   read -p "Если всё ок, нажми Enter для запуска..."
 
   rsync $RSYNC_OPTS -e ssh \
     "$SRC_MINT/$FOLDER" \
     "$USER@$HOST:$SRC_SERVER/$FOLDER"
 
-  echo "[OK] Синхронизация завершена: $FOLDER"
+  echo "$MSG_OK Синхронизация завершена: $FOLDER"
 }
+
 
 
 ###########################################################
 # Вызовы
 
-sync_folder "$LINUX_BOOKS"
-sync_folder "$LINUX_VIDEO"
+# sync_folder "$LINUX_BOOKS"
+# sync_folder "$LINUX_VIDEO"
+sync_folder "$OBSIDIAN"
